ac714d867395a5caaf6955b95adf8e68f635bfaf
Zulfiqurali95 <44014310+Zulfiqurali95@users.noreply.github.com>
2024-01-28 22:00:58 -0500

    Add files via upload

---
+++ SlotMachine/SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,18 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Zulfiqur on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
